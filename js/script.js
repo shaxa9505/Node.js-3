8683605a4d19a0ff1a613e693c37388c05ea1a85
shaxa_9505 <87860218+shaxa9505@users.noreply.github.com>
2022-07-26 00:18:59 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// GET ===== XHML 1-uslub  
+
+window.addEventListener("load", () => {
+
+    function req () {
+        const request = new XMLHttpRequest();
+        request.open("GET", "http://localhost:3000/people"); // biz get orqali surov qildik
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send()
+        // readystatechange
+        request.addEventListener("load", () => {
+            if(/* request.readyState == 4 &&  */request.status == 200) {
+                let data = JSON.parse(request.response) // JSON formatni jsga ugirib beradi
+    
+                // console.log(data);
+    
+                back(data)
+                
+            }
+            else {
+                console.error("Qayerdadir xatolik bor")
+            }
+        })
+
+        this.remove()
+    }
+
+    let click = document.querySelector(".click")
+    click.addEventListener("click", req, {once: true})
+
+
+
+
+    function back (data) {
+        data.forEach((item, index) => {
+            // console.log(item);
+            let card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                <img class="card_img" src=${item.photo} alt="">
+                <div class="name">${item.name} ${item.surname}</div>
+                <div class="jinsi">${item.jinsi}</div>
+                <div class="age">${item.age}</div>
+            `
+            document.querySelector(".app").appendChild(card)
+        })
+    }
+
+})
